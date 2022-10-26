c010283e3f1774e6b6df164cfb4eeb1f1cf3eb1b
JJoseph-bit <jay.master.flex001@gmail.com>
2022-10-25 21:24:07 -0700

    Task 1 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,19 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
- *  * main -  prints _putchar, followed by a new line
- *   * Return: Always 0 (Success)
- *    */
-int main(void)
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
 {
-		_putchar('H');
-			_putchar('o');
-				_putchar('l');
-					_putchar('b');
-						_putchar('e');
-							_putchar('r');
-								_putchar('t');
-									_putchar('o');
-										_putchar('n');
-											_putchar('\n');
+	char ch;
 
-												return (0);
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
 }
